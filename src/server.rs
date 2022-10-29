c4a838f1865eedc4314683195814cb6cf93c2474
Quentin Moss <quentinmoss@Hadouken.local>
2022-10-29 14:46:49 -0700

    Add request support

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -10,7 +13,26 @@ impl Server {
     // 'self' follows same ownership as normal variables. Takes ownership of the entire struct.
     // Struct will be deallocated when run exits.
     // If you do not want to deallocate the struct after exit, use reference
-    pub fn run(self) {
-        println!("Listening on {}", self.addr)
+    pub fn run(self) -> ! {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        println!("Listening on {}", &self.addr);
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    // Create buffer for io::read, 1024 in size. Takes u8, so just over 1kb
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => { 
+                            println!("Request: {}", String::from_utf8_lossy(&buffer));
+                        },
+
+                        Err(e) => println!("Error: {}", e)
+                    }
+                }
+                Err(e) => println!("Err: {}", e),
+            }
+        }
     }
 }
