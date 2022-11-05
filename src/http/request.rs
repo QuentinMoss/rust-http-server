a1d9fa1905b06ccf8935ebb56c8c0d53f1f29367
Quentin Moss <quentin.moss@protonmail.com>
2022-11-04 20:36:54 -0700

    Finish up request handling (#8)

--- src/http/request.rs
+++ src/http/request.rs
@@ -12,18 +12,45 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
+    // Set alias Error for ParseError
     type Error = ParseError;
 
-    // GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buff: &[u8]) -> Result<Self, Self::Error> {
-        match str::from_utf8(buf) {
-            Ok(request) => {},
-            Err(_) => return Err(ParseError);
-        }
+    // try_from will return Self::Error - Alias for ParseError
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        /*
+         * A verbose and less ideal approach. Example:
+         *
+         * match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+         *  Ok(request) => {}
+         *  Err(e) => return Err(e),
+         * }
+         *
+         * A better method using '?'
+         * This will look at the Result, if the Result is Ok, it will return
+         * If the Result is Err, it will return the error from our function: 
+         *
+         * The '?' will try to convert the error type it receives, if it does not match the error
+         * type the function is expected to return. 'from_utf8' returns its own Utf8Error 
+         *
+         * Example: str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+         *
+         * We can still make this cleaner by converting the from_utf8 error using 'From' trait, and
+         * map it to ParseError::InvalidEncoding
+         *
+         * Because we know that every time we get a Utf8Error, a InvalidEnding error is good, we can override the default errors
+         * using the method below
+         *
+         */
 
-        let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        // Cleanest Approach with '?', from_utf8 returns Utf8Error, we override with 'From' and
+        // return InvalidEncoding
+
+        let request = str::from_utf8(buf)?;
+
+        unimplemented!()
+        }
     }
-}
 
 pub enum ParseError {
     InvalidRequest,
@@ -43,16 +70,23 @@ impl ParseError {
     }
 }
 
-impl Debug for ParseError {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "", self.message())
+// Every time we get Utf8Error, let's return our defined InvalidEncoding error
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
-impl 
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message());
+        unimplemented!()
+    }
+}
+
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "", self.message())
+        write!(f, "{}", self.message())
     }
 }
 
