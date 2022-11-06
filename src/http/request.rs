c8b83fb1f453e269b82b00f01b755f65bdf1dfd3
Quentin Moss <quentin.moss@protonmail.com>
2022-11-05 18:16:31 -0700

    Add more advance query string parsing (#12)

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::{Method, MethodError};
+use super::QueryString;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -8,7 +9,7 @@ use std::str::Utf8Error;
 // Define lifetime for our buffer
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -98,7 +99,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
         if let Some(i) = path.find('?') {
             // Pad with +1 so ? isn't in query string
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
 
             // Assign everything before '?' to path
             path = &path[..i];
